0c468ebbd927123824fced656f7263f9b3adf2aa
haramkim-1 <kimharam@gmail.com>
2018-04-08 21:42:16 -0400

    setup and basic functionality

---
+++ EngineInterface/main.py
@@ -0,0 +1,45 @@
+def parse_command(instr, bot, pos):
+    if instr.startswith('action move'):
+        time = int(instr.split(' ')[-1])
+        x, y = bot.get_move(pos, time)
+        return 'place_move %d %d\n' % (x, y)
+    elif instr.startswith('update game field'):
+        fstr = instr.split(' ')[-1]
+        pos.parse_field(fstr)
+    elif instr.startswith('update game macroboard'):
+        mbstr = instr.split(' ')[-1]
+        pos.parse_macroboard(mbstr)
+    elif instr.startswith('update game move'):
+        pos.nmove = int(instr.split(' ')[-1])
+    elif instr.startswith('settings your_botid'):
+        myid = int(instr.split(' ')[-1])
+        bot.myid = myid
+        bot.oppid = 1 if myid == 2 else 2
+    elif instr.startswith('settings timebank'): 
+        bot.timebank = int(instr.split(' ')[-1])
+    elif instr.startswith('settings time_per_move'): 
+        bot.time_per_move = int(instr.split(' ')[-1])
+    return ''
+
+if __name__ == '__main__':
+    import sys
+    import uuid
+    from position import Position
+    from interfacebot import InterfaceBot
+
+    str_uuid = str(uuid.uuid1)
+    interface_pipe_path = str_uuid + '_pipe'
+    interface_lock_path = str_uuid + '_pipe.lock'
+
+    pos = Position()
+    bot = InterfaceBot(interface_pipe_path, interface_lock_path)
+    
+    while True:
+        try:
+            instr = input()
+        except Exception as e:
+            sys.stderr.write('error reading input')
+        outstr = parse_command(instr, bot, pos)
+        sys.stdout.write(outstr)
+        sys.stdout.flush()            
+            
