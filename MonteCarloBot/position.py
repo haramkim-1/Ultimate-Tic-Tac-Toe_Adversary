1ac2975f283b0bd06d5b3a7978e7174fefa6af85
Cosmo Viola <cviola14580@gmail.com>
2018-05-05 16:22:10 -0400

    Change position class to use floor division instead of true division.

--- MonteCarloBot/position.py
+++ MonteCarloBot/position.py
@@ -17,14 +17,14 @@ class Position:
         self.macroboard = [ int(f) for f in mblist ]
     
     def is_legal(self, x, y):
-        mbx, mby = x/3, y/3
+        mbx, mby = x//3, y//3
         return self.macroboard[3*mby+mbx] == -1 and self.board[9*y+x] == 0
 
     def legal_moves(self):
         return [ (x, y) for x in range(9) for y in range(9) if self.is_legal(x, y) ]
         
     def make_move(self, x, y, pid):
-        mbx, mby = x/3, y/3
+        mbx, mby = x//3, y//3
         self.macroboard[3*mby+mbx] = -1
         self.board[9*y+x] = pid
         
