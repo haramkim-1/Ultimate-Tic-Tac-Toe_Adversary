f7a7ae68be344cc5ebcec341f4ef1bb4b5d50dc7
Avani Bhargava <avanibhar@gmail.com>
2018-05-10 02:41:30 -0400

    more progress on minimax

--- MinimaxBot/minimaxbot.py
+++ MinimaxBot/minimaxbot.py
@@ -9,25 +9,28 @@ class MinimaxBot:
         # for each move, find minimax value and if greater than the best val,
         # change the best move and best val to this current move and value
         for move in lmoves:
-            curr_val = minimax(self, move, pos, 1, 3, True)
+            curr_val = minimax(move, pos, 1, 3, True)
             if curr_val > best_val
                 best_move = move
                 best_val = curr_val
         return best_move
 
-    def minimax(self, move, pos, depth, max_depth, my_turn):
-        # if game over, return value of board
+    def minimax(move, pos, depth, max_depth, my_turn):
 
         # if my turn, max
         # TODO check depth condition and if not met, use heuristic
         if depth == max_depth:
             # heuristic : return the number of boards won by whoever's turn it is
-            return pos.num_boards_won(pos.board, my_turn)
+            return pos.num_boards_won(self.myid, my_turn)
 
         # make a copy of the current position so we can make moves on it
         pos_copy = copy.deepcopy(pos)
         # get the legal next moves from this position
         lmoves = pos_copy.legal_moves()
+
+        if lmoves == []:
+            
+
         # for each of these possible next moves, make the move using pos copy
         make_move(pos_copy, move[0], move[1], self.myid)
 
@@ -35,7 +38,7 @@ class MinimaxBot:
             started = false
             for l in lmoves:
                 # set value to
-                val = minimax(self, l, pos_copy, depth+1, max_depth, False)
+                val = minimax(l, pos_copy, depth+1, max_depth, False)
                 if not started:
                     best_val = val
                 else:
@@ -47,7 +50,7 @@ class MinimaxBot:
             started = false
             for l in lmoves:
                 # set value to
-                val = minimax(self, l, pos_copy, depth+1, max_depth, True)
+                val = minimax(l, pos_copy, depth+1, max_depth, True)
                 if not started:
                     best_val = val
                 else:
