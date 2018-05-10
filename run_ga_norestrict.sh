d0c930b662b9bdc509a6386ddef6b65f29406078
Haram Kim <kimharam@gmail.com>
2018-05-09 22:57:23 -0400

    script updates

--- run_ga_norestrict.sh
+++ run_ga_norestrict.sh
@@ -5,6 +5,7 @@ DATE=$(date '+%Y-%m-%d-%H-%M')
 DIR=old_checkpoints/"$DATE"
 mkdir "$DIR"
 
+echo "Full run starting at time: ""$DATE"
 python -u Learn.py 2>"$DIR"/error_log.txt 1>"$DIR"/stdout_log.txt
 
 mv neat-checkpoint-* "$DIR"
