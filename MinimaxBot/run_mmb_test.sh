7265387ccd89281abc60b6117d7f39f2ddf1f39d
Haram Kim <kimharam@gmail.com>
2018-05-10 01:14:07 -0400

    script updated

---
+++ MinimaxBot/run_mmb_test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "aupdate game round 1
+update game move 1
+update game field 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+update game macroboard -1,-1,-1,-1,-1,-1,-1,-1,-1
+action move 10000" | python3 main.py
