288badccbe0eb60c74f2c186e81c66a7ec8a2d08
Haram Kim <kimharam@gmail.com>
2018-05-12 17:38:53 -0400

    viewer partially working

--- NetworkViewer/viewer.py
+++ NetworkViewer/viewer.py
@@ -2,7 +2,10 @@ import neat
 import visualize
 import os
 import sys
-import pickle
+
+def fake_eval_genomes(genomes, config):
+    for genome_id, genome in genomes:
+        genome.fitness = 1
 
 if __name__ == '__main__':
     pickle_path = sys.argv[1]
@@ -10,8 +13,13 @@ if __name__ == '__main__':
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
                          ".." + os.sep + "GeneticAlgorithm" + os.sep + 
                          "config-feedforward")
-    #unpickle
-    net = pickle.load(open( pickle_path, "rb" ))
+    #load checkpoint
+    checkpoint_path = sys.argv[1]
+    checkpoint = neat.Checkpointer.restore_checkpoint(checkpoint_path)
+    genome = checkpoint.population.run(fake_eval_genomes,1)
+
     #visualize
-    visualize.draw_net(config, net, True)
+    visualize.draw_net(config, checkpoint.best_genome, True)
+
+    
     
