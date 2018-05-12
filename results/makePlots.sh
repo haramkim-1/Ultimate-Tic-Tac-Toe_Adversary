2b14b9682ff5a3e94af0a2270b0e0d2e0294f07e
Haram Kim <kimharam@gmail.com>
2018-05-12 05:40:16 -0400

    plotting scripts

---
+++ results/makePlots.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+for path in *; do #loop over all isoRMSD directories
+    	[ -d "${path}" ] || continue # if not a directory, skip
+    	[ "old" != "${path}" ] || continue # skip old files directory
+	[ "plots" != "${path}" ] || continue # skip old files directory
+	dirname="$(basename "${path}")"	
+
+	echo "$path"
+
+	#copy gnuplot script into directory
+	cp gnuplotPlottingScript.gp $dirname
+
+	#enter directory
+	cd $dirname
+
+	#generate plotting data
+	cat stdout_log.txt | grep "Best fitness:" | awk '{ print $3 }' > best_fitness.tsv
+	cat stdout_log.txt | grep "average fitness:" | awk '{ print $4 ,"\t", $6 }' > average_fitness.tsv
+	
+	#run plotting script
+	gnuplot gnuplotPlottingScript.gp
+
+	#move output to plots directory
+	mv best.png ../plots/$(echo $dirname | sed -e 's/.*_//')_best.png
+	mv average.png ../plots/$(echo $dirname | sed -e 's/.*_//')_average.png
+	
+	#remove script
+	rm gnuplotPlottingScript.gp
+
+	#leave
+	cd ..
+done
