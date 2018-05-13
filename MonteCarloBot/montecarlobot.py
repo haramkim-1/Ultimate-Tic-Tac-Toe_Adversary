9982327a9a1b70e2f576c3bb244c8589f0b86189
Cosmo Viola <cviola14580@gmail.com>
2018-05-13 05:21:22 -0400

    Get monte carlo bot working?

--- MonteCarloBot/montecarlobot.py
+++ MonteCarloBot/montecarlobot.py
@@ -3,8 +3,7 @@ from random import randint
 import copy
 from enum import Enum
 import bisect
-import tkinter
-from tkinter import messagebox
+import sys
 
 class Node:
     moves = {}
@@ -13,6 +12,7 @@ class Node:
 
     def __init__(self, trials):
         self.trials = trials
+        moves = {}
 
 class Victory(Enum):
     WIN = 1
@@ -24,6 +24,11 @@ class MonteCarloBot:
     myid = -1
     oppid = -1
 
+    def __init__(self):
+        self.log = open("montecarlo_log.log", "a+")
+        self.log.write("\n\n")
+        self.log.flush
+
     def other_player(self, pid):
         if pid == self.myid:
             return self.oppid
@@ -31,6 +36,8 @@ class MonteCarloBot:
             return self.myid
 
     def monte_carlo_iter(self, pos, node, turn):
+        self.log.write("\t"+str(pos.legal_moves())+": ")
+        self.log.flush()
         vic = pos.checkMacroboardWinner()
         #print(pos)
         if vic == self.myid:
@@ -41,19 +48,24 @@ class MonteCarloBot:
         if not moves:
             return Victory.DRAW
         l = len(moves)
+        self.log.write("\t BEFORE MAKE CHILDREN: " + str(node.moves.keys())+": ")
+        self.log.flush()
         if not node.moves:
+            self.log.write("\t MAKING CHILDREN : ")
+            self.log.flush()
             for i in moves:
                 node.moves[i] = Node(2)
                 node.moves[i].wins[self.myid] = 1
                 node.moves[i].wins[self.oppid] = 1
+                node.moves[i].moves = {}
         weights = [0]*l
         total = 0
         for i in range(0,l):
             assert node.moves
-            try:
-                m = node.moves[moves[i]]
-            except Exception as e:
-                raise Exception(str(node.moves.keys()) + "\n\n\n" + str(moves))
+            #try:
+            m = node.moves[moves[i]]
+            #except Exception as e:
+            #    raise Exception(str(node.moves.keys()) + "\n\n\n" + str(moves))
             weights[i] = m.wins[turn]/m.trials
             total = total + weights[i]
         weights = [i/total for i in weights]
@@ -61,6 +73,8 @@ class MonteCarloBot:
             weights[i] = weights[i] + weights[i-1]
         num = random.random()
         choice = bisect.bisect(weights, num)
+        self.log.write(str(moves[choice])+"\n")
+        self.log.flush()
         pos.make_move(moves[choice], turn)
         result = self.monte_carlo_iter(pos, node.moves[moves[choice]], self.other_player(turn))
         node.trials = node.trials + 1
@@ -74,24 +88,33 @@ class MonteCarloBot:
             return Victory.DRAW
 
     def monte_carlo(self, pos, iterations):
+        self.log.write(str(pos.legal_moves())+"\n")
+        self.log.flush()
         root = Node(2)
         root.wins[self.myid] = 1
         root.wins[self.oppid] = 1
+        root.moves = {}
         #print("\n\n\niterating\n\n\n")
         for i in range(0,iterations):
-            if i > 0:
-                raise Exception("abc")
+            #if i > 0:
+            #    raise Exception("abc")
             #print("\n\n\nrunning iteration: "+ str(i) + "\n\n\n")
             self.monte_carlo_iter(copy.deepcopy(pos), root, self.myid)
+            self.log.write("\nFINISH ITER\n")
+            self.log.flush()
         
         #var = max(root.moves.items(), key=lambda item: item[1].wins[self.myid]/item[1].trials)[0]
         #print("\n\n\n\ntype: " + str(type(var)) + "\n\n\n\n")
-        return max(root.moves.items(), key=lambda item: item[1].wins[self.myid]/item[1].trials)[0]
+        move = max(root.moves.items(), key=lambda item: item[1].wins[self.myid]/item[1].trials)[0]
+        self.log.write(str(move)+"\n")
+        self.log.flush()
+        return move
 
     def get_move(self, pos, tleft):
         #return self.monte_carlo(pos, 1000)
         #print("GET MOVE RUNNING")
-        return self.monte_carlo(pos, 5)
+        num_moves = len(pos.legal_moves())
+        return self.monte_carlo(pos, 20*num_moves+300)
 
         #lmoves = pos.legal_moves()
         #rm = randint(0, len(lmoves)-1)
--- MonteCarloBot/position.py
+++ MonteCarloBot/position.py
@@ -4,32 +4,40 @@ class Position:
     def __init__(self):
         self.board = []
         self.macroboard = []
-        self.mLastX = -1
-        self.mLastY = -1
-
-    def __str__(self):
-        return "Position: " + ",".join(map(lambda x:str(x),self.board))
 
     def parse_field(self, fstr):
         flist = fstr.replace(';', ',').split(',')
-        self.board = [ int(f) for f in flist ]
+        self.board = [int(f) for f in flist]
 
     def parse_macroboard(self, mbstr):
         mblist = mbstr.replace(';', ',').split(',')
-        self.macroboard = [ int(f) for f in mblist ]
+        self.macroboard = [int(f) for f in mblist]
 
     def is_legal(self, x, y):
-        mbx, mby = x//3, y//3
+        mbx, mby = x // 3, y // 3
         return self.macroboard[3*mby+mbx] == -1 and self.board[9*y+x] == 0
 
     def legal_moves(self):
-        return [ (x, y) for x in range(9) for y in range(9) if self.is_legal(x, y) ]
+        return [(x, y) for x in range(9) for y in range(9)
+                if self.is_legal(x, y)]
 
     def make_move(self, move, pid):
         x, y = move
-        self.mLastX, self.mLastY = move
+        mbx, mby = x//3, y//3
+        next_mbx, next_mby = x%3, y%3
         self.board[9*y+x] = pid
-        self.updateMacroboard()
+        self.macroboard[3*mby+mbx] = self.getMicroboardWinner(mbx, mby)
+        if self.microboardFull(mbx, mby):
+            for macroX in range(0,2):
+                for macroY in range(0,2):
+                    if not self.microboardFull(macroX, macroY):
+                        self.macroboard[3*macroY + macroX] = -1
+        else:
+            for macroX in range(0,2):
+                for macroY in range(0,2):
+                    if self.macroboard[3*macroY+macroX] == -1:
+                        self.macroboard[3*macroY + macroX] = 0
+            self.macroboard[3*next_mbx + next_mby] = -1
 
     def get_board(self):
         return str.join(",", [str(i) for i in self.board])
@@ -37,7 +45,7 @@ class Position:
     def get_macroboard(self):
         return str.join(",", [str(i) for i in self.macroboard])
 
-    #from theAIGames engine
+    # from theAIGames engine
     def microboardFull(self, x, y):
         if x < 0 or y < 0:
             return True
@@ -49,43 +57,61 @@ class Position:
                     return False
         return True
 
-    def updateMacroboard(self):
-        for x in range(0, 3):
-            for y in range(0, 3):
-                winner = self.getMicroboardWinner(x, y)
-                self.macroboard[y*3+x] = winner
-        if (not self.microboardFull(self.mLastX%3, self.mLastY%3)):
-            self.macroboard[self.mLastX%3*3+self.mLastY%3] = -1
-        else:
-            for x in range(0, 3):
-                for y in range(0, 3):
-                    if (not self.microboardFull(x, y)):
-                        self.macroboard[y*3+x] = -1
+    # return number of microboards won by me if my_turn is true and number of
+    # microboards won by opponent if my_turn is false
+    #TODO: this counts draws as wins for the opponent
+    def num_boards_won(self, myid, my_turn):
+        won = 0
+        for x in range(0, 2):
+            for y in range(0, 2):
+                if self.getMicroboardWinner(x, y) == myid and my_turn:
+                    won += 1
+                if self.getMicroboardWinner(x, y) != myid and not my_turn:
+                    won += 1
+        return won
 
     def getMicroboardWinner(self, macroX, macroY):
-        startY = macroX*3
-        startX = macroY*3
+        startY = macroY*3
+        startX = macroX*3
         for x in range(startX, startX+3):
-            if (self.board  [startY*9+x] == self.board  [(startY+1)*9+x] and self.board  [(startY+1)*9+x] == self.board  [(startY+2)*9+x] and self.board  [(startY)*9+x] > 0):
-                return self.board  [startY*9+x]
+            if (self.board[startY*9+x] == self.board[(startY+1)*9+x] and
+                    self.board[(startY+1)*9+x] == self.board[(startY+2)*9+x]
+                    and self.board[(startY)*9+x] > 0):
+                return self.board[startY*9+x]
         for y in range(startY, startY+3):
-            if (self.board  [y*9+startX] == self.board  [y*9+startX+1] and self.board  [y*9+startX+1] == self.board  [y*9+startX+2] and self.board  [y*9+startX] > 0):
-                return self.board  [y*9+startX]
-        if (self.board  [startY*9+startX] == self.board  [(startY+1)*9+startX+1] and self.board  [(startY+1)*9+startX+1] == self.board  [(startY+2)*9+startX+2] and self.board  [startY*9+startX] > 0):
-            return self.board  [startY*9+startX]
-        if (self.board  [(startY+2)*9+startX] == self.board  [(startY+1)*9+startX+1] and self.board  [(startY+1)*9+startX+1] == self.board  [startY*9+startX+2] and self.board  [(startY+2)*9+startX] > 0):
-            return self.board  [(startY+2)*9+startX]
+            if (self.board[y*9+startX] == self.board[y*9+startX+1] and
+                    self.board[y*9+startX+1] == self.board[y*9+startX+2] and
+                    self.board[y*9+startX] > 0):
+                return self.board[y*9+startX]
+        if (self.board[startY*9+startX] == self.board[(startY+1)*9+startX+1]
+                and self.board[(startY+1)*9+startX+1] ==
+                self.board[(startY+2)*9+startX+2] and
+                self.board[startY*9+startX] > 0):
+            return self.board[startY*9+startX]
+        if(self.board[(startY+2)*9+startX] == self.board[(startY+1)*9+startX+1]
+                and self.board[(startY+1)*9+startX+1] ==
+                self.board[startY*9+startX+2]
+                and self.board[(startY+2)*9+startX] > 0):
+            return self.board[(startY+2)*9+startX]
         return 0
 
     def checkMacroboardWinner(self):
         for x in range(0, 3):
-            if (self.macroboard[x] == self.macroboard[1*3+x] and self.macroboard[1*3+x] == self.macroboard[2*3+x] and self.macroboard[0*3+x] > 0):
+            if (self.macroboard[x] == self.macroboard[1*3+x] and
+                    self.macroboard[1*3+x] == self.macroboard[2*3+x] and
+                    self.macroboard[0*3+x] > 0):
                 return self.macroboard[0*3+x]
         for y in range(0, 3):
-            if (self.macroboard[y*3+0] == self.macroboard[y*3+1] and self.macroboard[y*3+1] == self.macroboard[y*3+2] and self.macroboard[y*3+0] > 0):
+            if (self.macroboard[y*3+0] == self.macroboard[y*3+1] and
+                    self.macroboard[y*3+1] == self.macroboard[y*3+2] and
+                    self.macroboard[y*3+0] > 0):
                 return self.macroboard[x*3+0]
-        if (self.macroboard[0*3+0] == self.macroboard[1*3+1] and self.macroboard[1*3+1] == self.macroboard[2*3+2] and self.macroboard[0*3+0] > 0):
+        if (self.macroboard[0*3+0] == self.macroboard[1*3+1] and
+                self.macroboard[1*3+1] == self.macroboard[2*3+2] and
+                self.macroboard[0*3+0] > 0):
             return self.macroboard[0*3+0]
-        if (self.macroboard[2*3+0] == self.macroboard[1*3+1] and self.macroboard[1*3+1] == self.macroboard[0*3+2] and self.macroboard[2*3+0] > 0):
+        if (self.macroboard[2*3+0] == self.macroboard[1*3+1] and
+                self.macroboard[1*3+1] == self.macroboard[0*3+2] and
+                self.macroboard[2*3+0] > 0):
             return self.macroboard[2*3+0]
         return 0
